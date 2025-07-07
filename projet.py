5ec56feb185c393fd1a5520e2a3b37439d58337c
IhebZayati <ihebzayati@gmail.com>
2025-07-07 20:05:26 +0100

    last commit

--- projet.py
+++ projet.py
@@ -6,3 +6,9 @@ def moyenne (T):
 Data=[1,3,5]
 Moy=statistics.mean(Data)
 print("La moyenne est :", Moy)
+if Data :
+    print("La moyenne est :", Moy(Data))
+    print("Le min est :", Min(Data))
+    print("Le max est :", Max(Data))
+else:
+    print("Dossier vide")
